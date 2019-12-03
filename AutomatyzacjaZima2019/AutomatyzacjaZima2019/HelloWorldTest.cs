9dc2e9f0ec04d4c8bdf5c12fe495450d845b55ad
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2019-12-03 09:56:57 +0100

    initial commit

---
+++ AutomatyzacjaZima2019/AutomatyzacjaZima2019/HelloWorldTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace AutomatyzacjaZima2019
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void CanSayHello()
+        {
+            // aaa
+
+            // arrange
+            var a = 1;
+            var b = 2;
+            // act
+            var result = Function(a, b);
+
+            // assert
+            Assert.Equal(42, result);
+        }
+
+        private int Function(int a, int b)
+        {
+            return 42;
+        }
+    }
+}
