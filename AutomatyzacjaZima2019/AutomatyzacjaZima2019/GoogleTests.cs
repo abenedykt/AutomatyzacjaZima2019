91951d85a5adb710cb8b551793c7f3e80df8e510
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2019-12-03 15:28:15 +0100

    adding comment

--- AutomatyzacjaZima2019/AutomatyzacjaZima2019/GoogleTests.cs
+++ AutomatyzacjaZima2019/AutomatyzacjaZima2019/GoogleTests.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace AutomatyzacjaZima2019
@@ -26,6 +28,44 @@ namespace AutomatyzacjaZima2019
             var result = browser.FindElement(By.Id("wob_tm"));
 
             Assert.Equal("3", result.Text);
+        }
+
+        [Fact]
+        public void Can_add_new_comment()
+        {
+            browser.Navigate().GoToUrl("https://automatyzacja.benedykt.net");
+            var latestNote = browser.FindElement(By.CssSelector(".entry-title > a"));
+            latestNote.Click();
+
+            var comment = browser.FindElement(By.Id("comment"));
+            var exampleText = Faker.Lorem.Paragraph();
+            comment.SendKeys(exampleText);
+
+            var author = browser.FindElement(By.Id("author"));
+            var exampleAuthor = Faker.Name.FullName();
+            author.SendKeys(exampleAuthor);
+
+            var email = browser.FindElement(By.Id("email"));
+            var exampleEmail = Faker.Internet.Email();
+            email.SendKeys(exampleEmail);
+
+            MoveToElement(browser.FindElement(By.CssSelector("div.nav-links")));
+
+            browser.FindElement(By.Id("submit")).Submit();
+
+            var comments = browser.FindElements(By.CssSelector("article.comment-body"));
+            var myComments = comments
+                .Where(c => c.FindElement(By.CssSelector(".fn")).Text == exampleAuthor)
+                .Where(c => c.FindElement(By.CssSelector(".comment-content > p")).Text == exampleText);
+            
+            Assert.Single(myComments);
+        }
+
+        private void MoveToElement(IWebElement element)
+        {
+            Actions builder = new Actions(browser);
+            Actions moveTo = builder.MoveToElement(element);
+            moveTo.Build().Perform();
         }
 
         public void Dispose()
